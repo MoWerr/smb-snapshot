fafa2923fe74c86016b99aebf8b81028e572e5f4
MoWer <mbaran2601@gmail.com>
2021-03-17 23:10:27 +0100

    Used absolute paths in crontab file

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -28,7 +28,7 @@ function add_backup {
 # Adds cron entry to the crontab file
 function add_cron_entry {
     if [[ ! -z $2 && $2 > 0 && ! -z $3 ]]; then
-        echo "$3 rsnapshot -c ~/rsnapshot.conf $1" >> ~/cron-tasks
+        echo "$3 /usr/bin/rsnapshot -c /data/rsnapshot.conf $1" >> ~/cron-tasks
         msg "Added $1 cron entry"
     fi
 }
